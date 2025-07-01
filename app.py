05fb11476972d09a9a84f0ea094513912d9bfaba
efondo-aaronkyle <akyleefondo@gmail.com>
2025-07-01 10:41:03 +0800

    add function

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/name')
+def display_name():
+    return 'Hello, Kyle!'
+
 if __name__ == '__main__':
     app.run()
